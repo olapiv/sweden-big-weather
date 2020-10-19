e8d7ba12796dc64503cba4618550d29f280805f7
Vincent Lohse <vincent.lohse@live.de>
2020-10-19 23:57:18 +0200

    Removed server.on('upgrade')

--- node-server/app.js
+++ node-server/app.js
@@ -2,7 +2,6 @@ import http from 'http';
 import ws from 'ws';
 const { Server, Message, WebSocketClient } = ws;
 import express from 'express';
-import url from 'url';
 import { kafkaSubscribe } from './src/consumer.js';
 
 const PORT = 8001;
@@ -14,7 +13,7 @@ app.use(express.static('./static'));
 
 const server = http.createServer(app);
 
-const socketServer = new Server({ noServer: true });
+const socketServer = new Server({ server: server });
 socketServer.on('connection', socket => {
     console.log("Connected to Websocket!");
     socket.on('message', message => {
@@ -35,19 +34,6 @@ const send = (message) => {
         }
     );
 }
-
-server.on('upgrade', function upgrade(request, socket, head) {
-    const pathname = url.parse(request.url).pathname;
-    console.log("pathname: ", pathname);
-    if (pathname === '/ws') {
-        socketServer.handleUpgrade(request, socket, head, function done(ws) {
-            socketServer.emit('connection', ws, request);
-      });
-    } else {
-      socket.destroy();
-    }
-});
-
 server.listen(PORT);
 
 console.log(`socketServer listening: http://localhost:${PORT}`);
