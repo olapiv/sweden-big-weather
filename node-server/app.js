cdfa9194f07c83ad6acecba3ab1236115fdc9136
Vincent Lohse <vincent.lohse@live.de>
2020-10-22 20:50:37 +0200

    Using binami image for Kafka & Zookeeper; nodejs can now  connect to Kafka

--- node-server/app.js
+++ node-server/app.js
@@ -2,30 +2,15 @@ import http from 'http';
 import ws from 'ws';
 import express from 'express';
 import { kafkaSubscribe, kafkaFakeSubscribe } from './src/consumer.js';
+import { setUpClient, setupConsumer } from './src/kafka.js';
 
 const PORT = 8001;
+const KAFKA_TOPIC = "city-temperatures"
 
 const app = express();
-
-// Server static files
 app.use(express.static('./static'));
-
 const server = http.createServer(app);
-
 const socketServer = new ws.Server({ server: server });
-socketServer.on('connection', socket => {
-    console.log("Connected to Websocket!");
-    socket.on('message', message => {
-        console.log('Received message: ', message);
-    });
-    socket.send('Hey there!');
-
-    if (socketServer.clients.size === 1) {
-        kafkaFakeSubscribe('city-temperatures', broadcast)
-        // kafkaSubscribe('city-temperatures', broadcast);
-    }
-
-});
 
 const broadcast = (messageString) => {
     socketServer.clients.forEach(
@@ -34,6 +19,24 @@ const broadcast = (messageString) => {
         }
     );
 }
-server.listen(PORT);
 
+const kafkaClient = setUpClient();
+
+kafkaClient.refreshMetadata([KAFKA_TOPIC], (err) => {
+    if (err) {
+        throw err
+    }
+    console.log("Refreshed metadata!");
+
+    setupConsumer(kafkaClient, KAFKA_TOPIC, broadcast)
+
+    // This is actually not necessary:
+    socketServer.on('connection', socket => {
+        console.log("Connected to Websocket!");
+        socket.send('Hey there!');
+    });
+
+});
+
+server.listen(PORT);
 console.log(`socketServer listening: http://localhost:${PORT}`);
