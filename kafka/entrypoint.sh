ccc87eb51afcbbb62086a80fa1dd9a09c788104a
Vincent Lohse <vincent.lohse@live.de>
2020-10-22 21:05:14 +0200

    Removed kafka folder

--- kafka/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Creating a Kafka topic city-temperatures"
-$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic city-temperatures
-
-echo "Creating a Kafka topic grid-temperatures"
-$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic grid-temperatures
