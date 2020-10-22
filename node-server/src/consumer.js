96cc5e5886a0b3cdf0adbf34140b1982879535be
Vincent Lohse <vincent.lohse@live.de>
2020-10-22 20:53:01 +0200

    Slightly refactored consumerjs

--- node-server/src/consumer.js
+++ node-server/src/consumer.js
@@ -5,7 +5,7 @@ const { KafkaClient, Consumer, Message, Offset, OffsetFetchRequest, ConsumerOpti
 import sampleJSON from '../static/sampleData.json';
 import { filledData, getRandomInRange } from './fakeDataGenerator.js';
 
-const kafkaHost = 'localhost:9092';
+const KAFKA_HOST = 'kafka:9092';
 
 export function kafkaFakeSubscribe(topic, handleMessageFunc) {
     setInterval(
@@ -30,49 +30,43 @@ export function kafkaSubscribe(topic, handleMessageFunc) {
     // TODO: Think about having one continuous client, as a function parameter
 
     // New KafkaClient connects directly to Kafka brokers.
-    const client = new KafkaClient({ kafkaHost });
+    const client = new KafkaClient(KAFKA_HOST, 3000, 3000, true );
 
-    const topics = [{ topic: topic, partition: 0 }];
+    const topics = [{ topic: topic, partitions: 1 }];
     const options = { autoCommit: false, fetchMaxWaitMs: 1000, fetchMaxBytes: 1024 * 1024 };
 
     const consumer = new Consumer(client, topics, options);
 
-    consumer.on('error', function (err) {
+    consumer.on('error', (err) => {
         console.log('error', err);
     });
 
-    client.refreshMetadata(
-        [topic],
-        (err) => {
-            const offset = new Offset(client);
-
-            if (err) {
-                // throw err;
-                console.log("Error in client.refreshMetadata: ", err);
-                return
-            }
-
-            consumer.on('message', function (message) {
-                handleMessageFunc(message.value);
-            });
-
-            /*
-             * If consumer get `offsetOutOfRange` event, fetch data from the smallest(oldest) offset
-             */
-            consumer.on(
-                'offsetOutOfRange',
-                (topic) => {
-                    offset.fetch([topic], function (err, offsets) {
-                        if (err) {
-                            return console.error(err);
-                        }
-                        const min = Math.min.apply(null, offsets[topic.topic][topic.partition]);
-                        consumer.setOffset(topic.topic, topic.partition, min);
-                    });
-                }
-            );
+    client.refreshMetadata([topic], (err) => {
+        if (err) {
+            throw err;
         }
-    );
+
+        consumer.on('message', (message) => {
+            console.log("message.value: ", message.value);
+            handleMessageFunc(message.value);
+        });
+
+        const offset = new Offset(client);
+
+        // If consumer get `offsetOutOfRange` event, fetch data from the smallest(oldest) offset            
+        consumer.on(
+            'offsetOutOfRange',
+            (topic) => {
+                offset.fetch([topic], function (err, offsets) {
+                    if (err) {
+                        return console.error(err);
+                    }
+                    const min = Math.min.apply(null, offsets[topic.topic][topic.partition]);
+                    consumer.setOffset(topic.topic, topic.partition, min);
+                });
+            }
+        );
+    });
 
     // TODO: Perhaps return Consumer, so it can be cancelled when the browser closes
 }
